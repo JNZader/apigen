867b03bc589ebc8acadcba385cd1ce15a78d03dd
JNZader <javierzader@hotmail.com>
2026-01-28 10:34:49 -0300

    fix(server): use excludeName instead of exclude for auto-config
    Use string-based excludeName instead of class-based exclude to avoid
    compilation errors when DataSourceAutoConfiguration is not on classpath.

--- apigen-server/src/main/java/com/jnzader/apigen/server/ApiGenServerApplication.java
+++ apigen-server/src/main/java/com/jnzader/apigen/server/ApiGenServerApplication.java
@@ -2,17 +2,19 @@ package com.jnzader.apigen.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /**
  * Main entry point for the APiGen Server. Provides REST API for generating Spring Boot projects
  * from SQL schemas.
  *
  * <p>DataSource and JPA auto-configuration are excluded since this server doesn't use a database.
+ * Using excludeName instead of exclude to avoid compilation errors when JDBC is not on classpath.
  */
 @SpringBootApplication(
-        exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+        excludeName = {
+            "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
+            "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+        })
 public class ApiGenServerApplication {
 
     public static void main(String[] args) {
